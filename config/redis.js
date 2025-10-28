79d815a5599586f284bf494b90fb7f5f9b7ea3e2
h4r1z1b4mx <harizibamv@gmail.com>
2025-10-28 10:00:00 +0530

    created a two redis client for caching

---
+++ config/redis.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import { createClient } from "redis";
+dotenv.config();
+
+class RedisClient{
+    constructor (HOST, PORT, PASSWORD) {
+        this.client = createClient({
+            username:"default", 
+            password:PASSWORD,
+            socket: {
+                host: HOST, 
+                port: PORT, 
+            }, 
+        });
+
+        this.client.on("connect", () => {
+            console.log("Connected to Redis");
+        });
+        
+        this.client.on("ready", () => {
+            console.log("Redis is ready");
+        });
+
+        this.client.on("end", () => {   
+            console.log("Redis connection closed.");
+        });
+
+        this.client.on("error", (err) => {
+            console.log("Redis Client Error", err);
+        });
+    }
+}
+
+
+const redisA = new RedisClient(
+    process.env.REDIS_HOST1, 
+    process.env.REDIS_PORT1,
+    process.env.REDIS_PASSWORD1 
+).client;
+
+const redisB = new RedisClient(
+    process.env.REDIS_HOST2, 
+    process.env.REDIS_PORT2,
+    process.env.REDIS_PASSWORD2 
+).client;
+
